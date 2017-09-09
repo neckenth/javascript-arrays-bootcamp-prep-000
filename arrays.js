0e4ec552615c766b493b3f7366e930a41968a6e2
Nancy Eckenthal <nancy.eckenthal@gmail.com>
2017-09-09 15:53:08 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -10,17 +10,17 @@ var chocolateBars = [
 ]
 function addElementToBeginningOfArray(array, element) {
     const new_array = [element, ...array]
-    return new_array
+    return new_array;
 }
 function destructivelyAddElementToBeginningOfArray(array, element) {
     array.unshift(element)
-    return array
+    return array;
 }
 function addElementToEndOfArray(array, element) {
     const new_array = [array..., element]
-    return new_array
+    return new_array;
 }
 function destructivelyAddElementToEndOfArray(array, element) {
     array.push(element)
-    return array
+    return array;
 }
